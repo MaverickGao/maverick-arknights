df4f8838dc8302e722b950d2b95322f34c786cec
MaverickGao <zhiheng222@126.com>
2023-06-17 22:18:11 +0800

    first commit

---
+++ src/main/java/com/gzh/maverickarknights/MaverickArknightsApplication.java
@@ -0,0 +1,13 @@
+package com.gzh.maverickarknights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaverickArknightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaverickArknightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gzh/maverickarknights/MaverickArknightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzh.maverickarknights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaverickArknightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
