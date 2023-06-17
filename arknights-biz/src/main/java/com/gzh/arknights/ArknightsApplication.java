64dcebb95dbce045422021477edb3ccd0831eed6
MaverickGao <zhiheng222@126.com>
2023-06-17 23:00:18 +0800

    项目初始化

--- arknights-biz/src/main/java/com/gzh/arknights/ArknightsApplication.java
+++ arknights-biz/src/main/java/com/gzh/arknights/ArknightsApplication.java
@@ -1,13 +1,16 @@
-package com.gzh.maverickarknights;
+package com.gzh.arknights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author 高智恒
+ */
 @SpringBootApplication
-public class MaverickArknightsApplication {
-
+public class ArknightsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MaverickArknightsApplication.class, args);
+        SpringApplication.run(ArknightsApplication.class);
     }
-
 }
